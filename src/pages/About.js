ef4f51004304714c041909409d66caea70bda1cd
Akashdeepgupta124 <134071390+Akashdeepgupta124@users.noreply.github.com>
2024-06-28 21:30:23 +0530

    Your commit message here

---
+++ src/pages/About.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className='mb-20'>
+            <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'> About </h1>
+            <p className='mx-auto leading-relaxed text-base mb-4'>At [Your Blog Name], we are passionate coders and
+             tech enthusiasts dedicated to sharing our knowledge and experience with the
+             global coding community. Our team comprises seasoned professionals and coding 
+             aficionados who are eager to help you master the skills needed to succeed in the ever-evolving
+              tech industry </p>
+            <p className='mx-auto leading-relaxed text-base mb-4'>
+                We Offer
+               In-Depth Tutorials,
+               Our step-by-step tutorials cover a wide range of topics, from beginner-level introductions to advanced coding techniques. 
+                Whether you're looking to build your first web application with React or deploy a Node.js server,
+                our tutorials are designed to provide you with practical, hands-on experienced </p>
+            <p className='mx-auto leading-relaxed text-base mb-4'>
+             We invite you to explore our blog, try out our tutorials, and join our community. 
+            Subscribe to our newsletter to receive the latest updates directly to your inbox and never
+            miss out on new content. Connect with us on social media to stay engaged and be part of our
+            growing network of coding enthusiasts.
+            Thank you for visiting My Blog. We look forward to helping you on your coding journey! </p>
+            
+        </div>
+    )
+}
+
+export default About
---
+++ src/pages/Home.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className='mb-20'>
+            <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'>HEllo, welcome</h1>
+            <p className='mx-auto leading-relaxed text-base mb-4'>Hello, everyone! I'm Akash Deep Gupta, a passionate Front-End Developer,
+             and I'm thrilled to welcome you to my blog. Whether you're a fellow coder, a tech enthusiast, or someone just starting their
+             journey into the world of front-end development,
+             this blog is your new go-to resource. </p>
+            <p className='mx-auto leading-relaxed text-base mb-4'>With years of 
+            experience in building dynamic and responsive web applications, I've honed my skills in technologies like HTML, CSS, JavaScript, React, and more. My journey in the tech world has been filled with continuous learning and exciting challenges, 
+            and I'm here to share that knowledge and experience with you. </p>
+            <p className='mx-auto leading-relaxed text-base mb-4'>Dive into detailed tutorials and guides that cover everything from the basics of HTML and CSS to advanced JavaScript frameworks like React. My goal is to provide you with clear, step-by-step instructions that make learning fun and accessible.
+            Thank you for visiting my blog. I’m excited to embark on this learning journey with you and help you achieve your goals in front-end development. Let's code something amazing together!
+
+Happy coding!
+
+Akash Deep Gupta </p>
+            
+        </div>
+    )
+}
+
+export default Home
